87bbb8bae51ecaf424590217290188be34e9023f
Tilen Vatovec <tilen.vatovec@gmail.com>
2023-12-30 17:39:50 +0100

    Basic setup

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:@next/next/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'linebreak-style': ['error', 'unix'],
+  },
+};
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <h2>Welcome Back</h2>
+    </div>
+  );
+}
+
+export default Header;
