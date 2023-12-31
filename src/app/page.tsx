08ea2579926befc40408eb18cf252e4ecf615d04
Tilen Vatovec <tilen.vatovec@gmail.com>
2023-12-31 18:29:49 +0100

    Creating the dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Header from '@/components/Header';
+import DashboardCardsTop from '@/components/DashboardCardsTop';
+import DashboardMiddleSection from '@/components/DashboardMiddleSection';
 
 export default function Home() {
   return (
     <main className="bg-gray-100 min-h-screen">
       <Header />
+      <DashboardCardsTop />
+      <DashboardMiddleSection />
     </main>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 function Header() {
   return (
-    <div>
+    <div className="flex justify-between px-4 pt-4">
       <h2>Dashboard</h2>
-      <h2>Welcome Back</h2>
+      <h2>Most accurate gas prices</h2>
     </div>
   );
 }
